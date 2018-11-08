e8892ae5636a38afba670cf3b9e50580be06e913
sph-mn <sph@posteo.eu>
2018-11-08 19:37:41 +0000

    wip merge ytilitu into sph-info-dynamic

--- dynamic/root/assets/js/_time-calendar.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.define("ytilitu.time.calendar", function() {
-    var year_n_input = document.getElementById("year-n");
-    function year_change_event_handler() {
-        return window.location.assign("/dynamic/ytilitu/time/calendar/" + year_n_input.value);
-    }
-    return year_n_input.addEventListener("change", year_change_event_handler);
-});
